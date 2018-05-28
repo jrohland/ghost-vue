80a7aeeb788c1a8b4cec9f28949e679c6c6b0674
Jesse Rohland <jesse.rohland@gmail.com>
2018-05-27 22:16:46 -0400

    Adding devserver proxy for ghost content

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   lintOnSave: true,
   devServer: {
-    inline: true
+    inline: true,
+    proxy: {
+      '/content': {
+        target: 'http://localhost:2368',
+        changeOrigin: true
+      }
+    }
   }
 }
