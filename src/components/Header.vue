289b69cd63142d0f3c20789c5e5a62266e64594f
Jesse Rohland <jesse.rohland@gmail.com>
2018-06-04 16:11:29 -0400

    Adding webfonts and additional header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,13 +5,15 @@
     role="navigation"
     aria-label="main navigation">
 
-    <div class="navbar-brand">
-      <div class="title">
-        <router-link :to="{ name: 'posts' }">{{ title }}</router-link>
+    <div class="container">
+      <div class="navbar-brand column is-four-fifths">
+        <div class="title">
+          <router-link :to="{ name: 'posts' }">{{ title }}</router-link>
+        </div>
+        <div
+          class="description"
+          v-html="description" />
       </div>
-      <div
-        class="description"
-        v-html="description" />
     </div>
 
   </nav>
@@ -71,8 +73,8 @@ export default {
 <style scoped>
 .navbar {
   height: 150px;
-  -webkit-transition: height .5s, line-height .5s;
-  transition: height .5s, line-height .5s;
+  -webkit-transition: height .5s, line-height .5s, font-size .5s;
+  transition: height .5s, line-height .5s, font-size .5s;
 }
 
 .navbar.scrolled {
@@ -86,29 +88,45 @@ export default {
   margin: auto;
   display: flex;
   flex-direction: column;
+  padding: 0px;
 }
 
 .navbar.scrolled .navbar-brand {
   flex-direction: row;
+  justify-content: center;
 }
 
 .title {
+  font-family: 'Caveat', 'Cormorant Garamond', 'Avenir', Helvetica, Arial, sans-serif;
   display: flex;
   align-self: center;
-  font-size: 4rem;
+  font-size: 4.5rem;
   transition: font-size .5s;
-  margin-bottom: 0px;
+  margin-bottom: 0px !important;
+}
+
+.title a {
+  color: var(--header-title-color);
+}
+
+.title a:hover {
+  color: var(--header-title-hover-color);
 }
 
 .navbar.scrolled .title {
   font-size: 2rem;
   line-height: 60px !important;
-  margin-right: 15px;
+  margin-right: 30px;
+  margin-bottom: 0px;
 }
 
 .description {
   display: flex;
   align-self: center;
   text-align: center;
+}
+
+.navbar.scrolled .description {
+  font-size: .8rem;
 }
 </style>
