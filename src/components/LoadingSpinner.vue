ce6b31205187009c7729261bffeb242988931cac
Jesse Rohland <jesse.rohland@gmail.com>
2019-07-16 16:05:49 -0400

    Pulling loading spinner out of dependency because of security issue

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,28 +1,65 @@
 <template>
-  <vue-loading-spinner :size="sizeStr" />
+  <div
+    :style="styles"
+    class="spinner spinner--circle-5"
+  />
 </template>
-
 <script>
-import Circle5 from 'vue-loading-spinner/src/components/Circle5'
-
 export default {
-  components: {
-    'vue-loading-spinner': Circle5
-  },
-
   props: {
     size: {
       type: Number,
       default: 40
     }
   },
-
   computed: {
-    sizeStr: {
-      get () {
-        return this.size + 'px'
+    styles () {
+      return {
+        width: this.size + 'px',
+        height: this.size + 'px'
       }
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+  .spinner {
+    transform: translate3d(0, 0, 0);
+    position: relative;
+    background: rgba(25, 165, 152, 0.5);
+    border-radius: 50%;
+    overflow: hidden;
+  }
+  .spinner:before, .spinner:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+  .spinner:before {
+    animation: circle-5-spinner 3s infinite;
+    transform-origin: 0 50%;
+    left: 0;
+    background: #c7efcf;
+  }
+  .spinner:after {
+    animation: circle-5-spinner 3s 0.2s reverse infinite;
+    transform-origin: 100% 50%;
+    right: 0;
+    background: #eef5db;
+  }
+  @keyframes circle-5-spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(360deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+</style>
